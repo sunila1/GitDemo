e0b4ddec74b0a9b8989b3c0bf01f31cf5e5176cc
Sunila <sunilaotta@gmail.com>
2022-05-31 16:19:42 +0530

    Commit message

---
+++ src/main/java/demo/maven/apitesting.java
@@ -0,0 +1,19 @@
+package demo.maven;
+
+import org.testng.annotations.Test;
+
+public class apitesting {
+	
+		@Test
+		public void mobilebrowser()
+		{
+			System.out.println("here i am mobilebroser");
+		}
+		@Test
+		public void mobilelogin()
+		{
+			System.out.println("here i am mobilelogin");
+		}
+	}
+
+
---
+++ src/main/java/demo/maven/seleniumjava.java
@@ -0,0 +1,17 @@
+package demo.maven;
+
+import org.testng.annotations.Test;
+
+public class seleniumjava {
+	@Test
+	public void webbrowser()
+	{
+		System.out.println("here i am webbroser");
+	}
+	@Test
+	public void weblogin()
+	{
+		System.out.println("here i am weblogin");
+	}
+
+}
